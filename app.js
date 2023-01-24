97ad3d21bae300119b5a11d85da71317e2f28b51
Ugheoke <ougheoke@una.edu>
2023-01-24 16:42:46 -0600

    commit 2

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000; 
 
+//set the view engine to ejs
+
+app.set('view engine', 'ejs');
+
+let myName = 'Omonegho';
 
 app.get('/', (req, res) => {
-  res.send('Hello World, is it November?')
+//   res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
+
+// res.send(`<h3> Hi, ${ myName }</h3>`)
+
+})
+
+app.get('/show', (req, res) => {
+    // res.sendFile('index.html' , { root : __dirname});
+  })
+
+app.get('/ejs', (req, res) => {
+    res.render('index');
 })
 
 console.log('in the node console');
