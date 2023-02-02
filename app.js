48795c42304f5bb793d2cd28af08ef08f2b92f68
ougheoke <ougheoke@una.edu>
2023-02-02 13:56:31 -0600

    ejs corrected

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ app.get('/show', (req, res) => {
   console.log('before app dot get to /ejs')
 
 app.get('/ejs', (req, res) => {
-    res.render('index');
+  //ejs stuff here
+  //use res.render to load up an ejs view file
+
+  console.log("in res render /ejs:", myName);
+    res.render('index',{ myName: myName}); // left one is ejs, right is node
+    console.log("after res render /ejs:", myName);
 })
 
 app.get('/name', (req, res) => {
@@ -35,7 +40,3 @@ console.log('in the node console');
 app.listen(PORT, () => {
     console.log(`Server is running & listening on port: ${PORT}`);
   });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
